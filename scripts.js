b620ff49b5de53ebfb27cc3f4e89d55cc0eb649a
Janizs Kristóf <janizs.kristof11@gmail.com>
2024-10-12 19:49:32 +0200

    added theme switch

--- scripts.js
+++ scripts.js
@@ -1,10 +1,29 @@
+const themeToggle = document.querySelector(".theme-toggle");
+const circle = document.querySelector(".circle");
+
 function setTheme() {
+    themeToggle.classList.toggle("active")
     const root = document.documentElement;
     const newTheme = root.className === 'light' ? 'dark' : 'light';
     root.className = newTheme;
+    if (newTheme === "dark"){
+        circle.style.background = "white";
+        themeToggle.style.background = "black";
+        themeToggle.style.justifyContent = 'flex-start';
+        
+    }
+    else{
+        circle.style.background = "black";
+        themeToggle.style.background = "white";
+        themeToggle.style.justifyContent = 'flex-end'
+    }
 }
 
-document.querySelector('.theme-toggle').addEventListener('click', setTheme)
+
+//Togge theme
+
+//themeToggle.addEventListener('click', setTheme);
+themeToggle.addEventListener('click', setTheme);
 
 
 window.onload = ()=>{
